de6397c563935aa210eb247740b77d30c1dc88ec
mohamed3laa <mohamed3laa3467@gmail.com>
2025-03-27 02:42:35 +0200

    make some homepage components , make sign-in ,signup ui , sidebar ui

--- lib/utils.ts
+++ lib/utils.ts
@@ -134,25 +134,24 @@ export function countTransactionCategories(
   transactions: Transaction[]
 ): CategoryCount[] {
   const categoryCounts: { [category: string]: number } = {};
-  let totalCount = 0;
 
-  // Iterate over each transaction
-  transactions &&
-    transactions.forEach((transaction) => {
-      // Extract the category from the transaction
-      const category = transaction.category;
+  transactions.forEach((transaction) => {
+    const category = transaction.category;
 
-      // If the category exists in the categoryCounts object, increment its count
-      if (categoryCounts.hasOwnProperty(category)) {
-        categoryCounts[category]++;
-      } else {
-        // Otherwise, initialize the count to 1
-        categoryCounts[category] = 1;
-      }
+    if (categoryCounts[category]) {
+      categoryCounts[category]++;
+    } else {
+      categoryCounts[category] = 1;
+    }
+  });
 
-      // Increment total count
-      totalCount++;
-    });
+  // Convert object to an array of { category, count }
+  return Object.entries(categoryCounts).map(([category, count]) => ({
+    category,
+    count,
+  }));
+}
+
 
   // Convert the categoryCounts object to an array of objects
   const aggregatedCategories: CategoryCount[] = Object.keys(categoryCounts).map(
